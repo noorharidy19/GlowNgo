2182fb730ebc0c430539498d94e4aa3adb0407cd
noorharidy19 <noor2206987@miuegypt.edu.eg>
2024-06-18 19:34:50 +0300

    nav routes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const adminRouter = require('./routes/adminroute');
 const adminPRouter = require('./routes/adminProute');
+const navRouter = require('./routes/navRoute');
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,7 +15,7 @@ app.use(bodyParser.json());
 app.use(express.static("public", { maxAge: "7d" }));
 app.use(adminRouter);
 app.use(adminPRouter);
-
+app.use(navRouter);
 app.set('view engine', 'ejs');
 
 // Define a router and a route handler
---
+++ routes/navRoute.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/Home', (req, res) => {
+    res.render('Home');
+});
+router.get('/Products', (req, res) => {
+    res.render('Products');
+});
+router.get('/About', (req, res) => {
+    res.render('About');
+});
+router.get('/profile', (req, res) => {
+    res.render('myprofile');
+});
+module.exports = router;
