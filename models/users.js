07d7cd3ec1d21a282543b84bf623d799e70fbc00
noorharidy19 <noor2206987@miuegypt.edu.eg>
2024-06-21 00:04:37 +0300

    schema update

--- models/users.js
+++ models/users.js
@@ -30,6 +30,10 @@ const userSchema = new mongoose.Schema({
     acceptedTerms: {
         type: Boolean,
         required: true
+    },
+    type: {
+        type: String,
+        default: "customer"
     }
 });
 
