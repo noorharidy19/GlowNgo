155b4c196fc0f95bd4a00b70ee624f9e949066d2
nour <nourshawaf1@gmail.com>
2024-06-21 01:28:55 +0300

    user admin

--- routes/adminUroute.js
+++ routes/adminUroute.js
@@ -6,11 +6,7 @@ const router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-const prod = require("../controllers/prod");
+const user = require("../controllers/user");
 
-router.get('/AdminProducts', prod.getProducts);
-router.put('/edit/:id', prod.updateProduct);
-router.delete('/delete/:id',prod.deleteProducts);
-router.post('/addProduct', prod.Addproducts);
 
 module.exports = router;
