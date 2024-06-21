7956dd09736e86079cccb940500cfa95cb954a4d
ShadenEssam <shaden2208768@miuegypt.edu.eg>
2024-06-21 22:04:43 +0200

    Profile

--- routes/profile.js
+++ routes/profile.js
@@ -5,19 +5,27 @@ const userController = require('../controllers/profile');
 // Update username
 router.put('/:email/username', userController.updateUsername);
 
-// Add new address
-router.post('/:email/address', userController.addAddress);
-
-// Update an address by index
-router.put('/:email/address/:index', userController.updateAddress);
-
-// Delete an address by index
-router.delete('/:email/address/:index', userController.deleteAddress);
+// Add or update address
+router.put('/:email/address', userController.updateAddress);
 
 // Change user password
 router.put('/:email/password', userController.changePassword);
 
 // Update phone
 router.put('/:email/phone', userController.updatePhone);
+
+// Route to get profile information
+router.get('/profile/:email', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.params.email });
+        if (!user) {
+            return res.status(404).json({ msg: 'User not found' });
+        }
+        res.render('profile', { user });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
